5b5790e149a0fe15e832d0bc044e39ece038fd70
patelheta <patelheta385@gmail.com>
2022-11-01 20:53:33 -0700

    Adding post request in tinyapp

--- express_server.js
+++ express_server.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+const generateRandomString = function() { };
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
