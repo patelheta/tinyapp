a0463e2e4abd6c370c019c9a1f7f3383c5571fc5
patelheta <patelheta385@gmail.com>
2022-11-01 21:33:28 -0700

    Implement redirect longUrl

--- express_server.js
+++ express_server.js
@@ -3,7 +3,9 @@ const app = express();
 const PORT = 8080; // default port 8080
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
-const generateRandomString = function() { };
+const generateRandomString = function() {
+  return Math.random().toString(36).substring(2, 7);
+};
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -11,7 +13,9 @@ const urlDatabase = {
 };
 app.post("/urls", (req, res) => {
   console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  let id = generateRandomString();
+  urlDatabase[id] = req.body.longURL;
+  res.redirect(`/urls/${id}`); // Respond with 'Ok' (we will replace this)
 });
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
@@ -19,6 +23,10 @@ app.get("/urls/new", (req, res) => {
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
+});
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id];
+  res.redirect(longURL);
 });
 
 app.get("/urls", (req, res) => {
